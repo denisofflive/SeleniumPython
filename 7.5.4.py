a435743903246e16771a8d8b33dd7c2868633835
denis <denisofflive@mail.ru>
2024-02-14 17:10:21 +0300

    7.5.4

---
+++ 7.5.4.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+with webdriver.Chrome() as browser:
+    browser.get('http://parsinger.ru/window_size/1/index.html')
+    browser.set_window_size(555 + 16, 555 + 138)
+    print("Результат:", browser.find_element(By.ID, 'result').text)
