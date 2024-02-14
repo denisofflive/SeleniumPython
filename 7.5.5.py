e10a38909c37c26b8242ea5e686996a407ac280f
denis <denisofflive@mail.ru>
2024-02-14 18:15:23 +0300

    7.5.5

--- 7.5.5.py
+++ 7.5.5.py
@@ -24,8 +24,8 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 url = 'http://parsinger.ru/window_size/2/index.html'
-size_x = [516, 648, 680, 701, 730, 750, 805, 820, 855, 890, 955, 1000]
-size_y = [270, 300, 340, 388, 400, 421, 474, 505, 557, 600, 653, 1000]
+size_x = [616, 648, 680, 701, 730, 750, 805, 820, 855, 890, 955, 1000]
+size_y = [300, 330, 340, 388, 400, 421, 474, 505, 557, 600, 653, 1000]
 
 with webdriver.Chrome() as browser:
     browser.get(url)
