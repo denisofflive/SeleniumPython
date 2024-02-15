ec05b46ffbb411fd76b9b070261725e74e358de9
denis <denisofflive@mail.ru>
2024-02-15 17:46:08 +0300

    9.7

--- 9 Drag and drop/9.7.py
+++ 9 Drag and drop/9.7.py
@@ -1,17 +1,18 @@
-import time
-from selenium.webdriver import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+
 
 with webdriver.Chrome() as browser:
-    browser.get('https://parsinger.ru/draganddrop/3/index.html')
-    time.sleep(1)
-    drag = browser.find_element(By.ID, 'block1')
+    browser.get(url='https://parsinger.ru/draganddrop/3/index.html')
     actions = ActionChains(browser)
-    lst = [10]*400
-    actions.click_and_hold(drag)
-    for x in lst:
-        actions.move_by_offset(x, 0)
-    actions.perform()
-    time.sleep(3)
+
+    block = browser.find_element(By.ID, 'block1')
+    actions.click_and_hold(block).perform()
+
+    for point in browser.find_elements(By.CLASS_NAME, 'controlPoint'):
+        actions.move_to_element(point).perform()
+
+    actions.click(block).perform()
+    print("Секретный код:", browser.find_element(By.ID, 'message').text)
+
